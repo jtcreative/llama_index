e4c1dc59e7e66ed47913db72162b306460189c8c
Giorch <69166473+Giorch@users.noreply.github.com>
2025-05-19 18:50:49 -0400

    Added a chat engine and a memory buffer
    This should fix the chatbot being stateless and forgetting previous messages.

--- backend/main.py
+++ backend/main.py
@@ -1,5 +1,7 @@
 import logging
 import sys
+import os
+sys.path.append(os.path.dirname(__file__))
 import translation_model
 import chromadb
 
@@ -12,6 +14,8 @@ from langdetect import detect
 from llama_index.core import VectorStoreIndex, SimpleDirectoryReader
 from llama_index.core.response_synthesizers import get_response_synthesizer
 from llama_index.core.query_engine import RetrieverQueryEngine
+from llama_index.core.chat_engine import CondenseQuestionChatEngine
+from llama_index.core.memory import ChatMemoryBuffer
 from llama_index.core.retrievers import VectorIndexRetriever
 from chatbot import Settings, client
 
@@ -24,8 +28,9 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
+chat_engines = {}
 translator = translation_model.create_text_translation_client_with_credential()
-documents = SimpleDirectoryReader("data").load_data()
+documents = SimpleDirectoryReader("backend/data").load_data()
 # db  = chromadb.PersistentClient(path="./chroma_db")
 # chroma_collection = db.get_or_create_collection("MediChat")
 # vector_store = ChromaVectorStore(chroma_collection=chroma_collection)
@@ -36,12 +41,19 @@ retriever = VectorIndexRetriever(index=index)
 query_engine = RetrieverQueryEngine(retriever=retriever, response_synthesizer=response_synthesizer)
 
 class QueryRequest(BaseModel):
+    session_id: str
     query: str
 
 @app.post("/query")
 
 async def query_llama(request: QueryRequest):
+
+    session_id = request.session_id
+
       # Step 1: Detect the language of the question
+    if session_id not in chat_engines:
+        chat_engines[session_id] = CondenseQuestionChatEngine.from_defaults(query_engine=query_engine, lm=client, memory= ChatMemoryBuffer.from_defaults())
+        
     language = detect(request.query)
 
     # Step 2: If not English, translate the question
@@ -51,9 +63,9 @@ async def query_llama(request: QueryRequest):
         translated_question = request.query
 
     # Step 3: Use LlamaIndex to answer the question
-    answer_in_english = query_engine.query(translated_question)
+    answer_in_english = chat_engines[session_id].chat(translated_question)
 
-      # #Step 4: Translate the answer back to the original language
+    # #Step 4: Translate the answer back to the original language
     if language != 'en':
         answer_in_user_language = translator.translate(body=[answer_in_english.response], from_language='en', to_language=[language])[0].translations[0].text
     else:
