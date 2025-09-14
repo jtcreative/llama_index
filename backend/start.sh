46fab70521bad4c75d228dade249aaa1afc304c8
Giorch <69166473+Giorch@users.noreply.github.com>
2025-09-14 12:24:20 -0400

    Update start.sh

--- backend/start.sh
+++ backend/start.sh
@@ -1,7 +1,3 @@
 #!/bin/bash
-if [ ! -f ./lid.176.bin ]; then
-    echo "Downloading FastText language model..."
-    curl -L -o lid.176.bin https://dl.fbaipublicfiles.com/fasttext/supervised-models/lid.176.bin
-    echo "Download complete."
-fi
+
 uvicorn main:app --host 0.0.0.0 --port 10000
