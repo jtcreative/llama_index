fd4916245121a5908d6dd17c5b5d655669d43a8a
Giorch <69166473+Giorch@users.noreply.github.com>
2025-09-14 23:12:19 -0400

    Removed compressed files from version control

--- backend/start.sh
+++ backend/start.sh
@@ -4,8 +4,13 @@
 #     curl -L -o lid.176.bin https://dl.fbaipublicfiles.com/fasttext/supervised-models/lid.176.bin
 #     echo "Download complete."
 # fi
-if [ ! -d ./chroma_db ]; then
-    echo "Extracting Chroma index..."
-    unzip compressed_index/chroma_db.zip -d chroma_db
-fi
+mkdir -p chroma_db
+
+# Download the zip if it doesn't already exist
+echo "Downloading ChromaDB index..."
+curl -L -o chroma_db/chroma_db.zip \
+https://medichatblobstorage.blob.core.windows.net/indexes/chroma_db.zip
+echo "Extracting ChromaDB index..."
+unzip -o chroma_db/chroma_db.zip -d chroma_db
+
 uvicorn main:app --host 0.0.0.0 --port 10000
